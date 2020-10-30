ef949cde3616058c87f16ec260d458a6811af4c1
Ray Bray <rbray.bray2@gmail.com>
2020-10-29 23:49:32 -0400

    added search form

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
+
 class Home extends Component {
+    state = {
+        search: ""
+    }
+
+    handleInputChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value,
+        })
+        
+    }
+
+
     render() {
         return (
-            <div>
-               <h1>This is my home page</h1> 
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-3"></div>
+                    <div className="col-sm-6 text-center">
+                        <form>
+                            <div className="form-group">
+                            <label htmlFor="exampleInputSearch">Search</label>
+                            <input
+                                type="search"
+                                className="form-control"
+                                id="exampleInputSearch"
+                                name="search"
+                                value={this.state.search}
+                                onChange={this.handleInputChange}
+                            />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-sm-3"></div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <p className="text-center">This is where the table should go.</p>
+                    </div>
+                </div>
             </div>
         );
     }
