8c351f9b12b2e1111bbe2b5953dbdafe19d3f8ea
Ray Bray <rbray.bray2@gmail.com>
2020-10-30 23:42:14 -0400

    app works

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -22,6 +22,7 @@ class Home extends Component {
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
+    this.myFunction();
     this.setState({
       [name]: value,
     });
@@ -82,6 +83,25 @@ class Home extends Component {
         if (count === 0 && direction === "ascending") {
           direction = "descending";
           switching = true;
+        }
+      }
+    }
+  }
+
+  myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("exampleInputSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
       }
     }
